874b157137aaee79bc4eb35a51316deb155b394a
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-10 18:51:06 +0530

    handling HTTP post request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id : 1, name:'course1'},
     {id : 2, name:'course2'},
@@ -11,6 +13,10 @@ app.get('/',(req,res)=>{
     res.send('Hello world!');
 });
 
+// app.get('/',(req,res)=>{
+//     return res.json('Hello world');
+// });
+
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
 });
@@ -19,6 +25,17 @@ app.get('/api/courses:id',(req,res)=>{
 
     const course = courses.find(c=>c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course with the given id was not found ');
+    res.send(course);
+});
+
+
+//post request
+app.post('/api/courses',(req,res)=>{
+    const course ={
+        id : courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
     res.send(course);
 });
 
